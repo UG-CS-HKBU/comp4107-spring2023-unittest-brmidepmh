f78f086c157727af2ae8b93fd80d3aa75dbadfad
brmidepmh <110738094+brmidepmh@users.noreply.github.com>
2023-03-30 22:25:13 +0800

    fake(not yet finished)

--- src/main/kotlin/UnitTest.kt
+++ src/main/kotlin/UnitTest.kt
@@ -18,4 +18,81 @@ class UnitTest {
             assertTrue(heroes.size == 2)
         }
     }
+
+    @Test
+    fun testCaoDodgeAttack() {
+        monarchHero = CaoCao()
+        for (i in 1..7) {
+            heroes.add(NoneMonarchFactory.createRandomHero())
+        }
+        assertTrue(monarchHero.dodgeAttack())
+    }
+
+    @Test
+    fun testBeingAttacked() {
+        for (hero in heroes) {
+            val spy = object : Hero(MinisterRole()) {
+                override val name = hero.name
+                override fun beingAttacked() {
+                    hero.beingAttacked()
+                    assertTrue(hero.hp >= 0)
+                }
+            }
+            for(i in 1..10)
+                spy.beingAttacked()
+        }
+    }
+
+    object FakeNonmonarchFactory : GameObjectFactory{
+        var count = 0
+        var last: WeiHero? = null
+        init {
+            monarchHero = CaoCao()
+        }
+        override fun getRandomRole(): Role {
+            return MinisterRole()
+        }
+        override fun createRandomHero(): Hero {
+            val hero = when(count++) {
+                0->SimaYi(getRandomRole())
+                1->XiaHouyuan(getRandomRole())
+                else->XuChu(getRandomRole())
+            }
+            val cao = monarchHero as CaoCao
+            if (last == null)
+                cao.helper = hero
+            else
+                last!!.setNext(hero)
+            last = hero
+            return hero
+        }
+    }
+
+    object FakeMonarchFactory : GameObjectFactory{
+        override fun createRandomHero() : Hero {
+            monarchHero = CaoCao()
+            return monarchHero as CaoCao
+        }
+        override fun getRandomRole(): Role {
+            return MonarchRole()
+        }
+    }
+
+    class CaoCaoUnitTest {
+        @Test
+        fun testCaoDodgeAttack(){
+            heroes.add(FakeMonarchFactory.createRandomHero())
+            for (i in 0..2) {
+                var hero = FakeNonmonarchFactory.createRandomHero()
+                hero.index = heroes.size;
+                heroes.add(hero)
+            }
+            for (hero in heroes) {
+                hero.beingAttacked()
+                hero.templateMethod()
+            }
+//            assert(heroes[0].dodgeAttack())
+
+        }
+    }
 }
